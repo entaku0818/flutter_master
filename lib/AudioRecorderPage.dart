566598d36feb2285599418e474ad27d49a91f9db
entaku <entaku19890818@gmail.com>
2024-04-12 10:06:10 +0900

    add

--- lib/AudioRecorderPage.dart
+++ lib/AudioRecorderPage.dart
@@ -1,72 +1,50 @@
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_sound/flutter_sound.dart';
+import 'package:flutter_master/audio_player.dart';
+import 'package:flutter_master/audio_recorder.dart';
 
 class AudioRecorderPage extends StatefulWidget {
+  const AudioRecorderPage({super.key});
+
   @override
-  _AudioRecorderPageState createState() => _AudioRecorderPageState();
+  State<AudioRecorderPage> createState() => _AudioRecorderPageState();
 }
 
 class _AudioRecorderPageState extends State<AudioRecorderPage> {
-  FlutterSoundRecorder _recorder = FlutterSoundRecorder();
-  bool _isRecording = false;
+  bool showPlayer = false;
+  String? audioPath;
 
   @override
   void initState() {
+    showPlayer = false;
     super.initState();
-    _recorder.openRecorder();
-  }
-
-  @override
-  void dispose() {
-    _recorder.closeRecorder();
-    super.dispose();
-  }
-
-  Future<void> startRecording() async {
-    await _recorder.startRecorder(toFile: 'audio.aac');
-    setState(() {
-      _isRecording = true;
-    });
-  }
-
-  Future<void> stopRecording() async {
-    await _recorder.stopRecorder();
-    setState(() {
-      _isRecording = false;
-    });
-  }
-
-  Future<void> playRecording() async {
-    FlutterSoundPlayer _player = FlutterSoundPlayer();
-    await _player.openPlayer();
-    await _player.startPlayer(fromURI: 'audio.aac');
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Audio Recorder'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: _isRecording ? null : startRecording,
-              child: Text('Start Recording'),
-            ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: _isRecording ? stopRecording : null,
-              child: Text('Stop Recording'),
-            ),
-            SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: playRecording,
-              child: Text('Play Recording'),
-            ),
-          ],
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: showPlayer
+              ? Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: AudioPlayer(
+                    source: audioPath!,
+                    onDelete: () {
+                      setState(() => showPlayer = false);
+                    },
+                  ),
+                )
+              : Recorder(
+                  onStop: (path) {
+                    if (kDebugMode) print('Recorded file path: $path');
+                    setState(() {
+                      audioPath = path;
+                      showPlayer = true;
+                    });
+                  },
+                ),
         ),
       ),
     );
